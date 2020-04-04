725b064b60d79fe8ceff8768de4f2ceaf99ec9be
泽兑 <48466662+lqxzd@users.noreply.github.com>
2020-04-04 19:16:46 +0800

    Update remove_bg.py

--- remove_bg.py
+++ remove_bg.py
@@ -1,7 +1,8 @@
 from removebg import *
 
 #将your API Key替换成获得的API Key
-rmbg = RemoveBg("your API Key") 
+#API Key长时间不用是会变的哦
+rmbg = RemoveBg("your API Key", "error.log") 
 
 #将you path替换成照片路径
 rmbg.remove_background_from_img_file("you path")
